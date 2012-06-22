1fd2eec19ad722d33ed224b3ec4fc82badb820d6
Marco Raddatz <github@marcoraddatz.com>
2012-06-22 15:22:21 +0200

    tabs to spaces

--- controllers/Main.controller.php
+++ controllers/Main.controller.php
@@ -433,39 +433,39 @@ abstract class Main {
   public function show() {
     $this->oSmarty->setCaching(SmartySingleton::CACHING_LIFETIME_SAVED);
 
-		if (isset($this->_aRequest['type']) && 'json' == $this->_aRequest['type'])
-			return $this->_showJSON();
+    if (isset($this->_aRequest['type']) && 'json' == $this->_aRequest['type'])
+      return $this->_showJSON();
 
-		elseif (isset($this->_aRequest['type']) && 'xml' == $this->_aRequest['type'])
-			return $this->_showXML();
+    elseif (isset($this->_aRequest['type']) && 'xml' == $this->_aRequest['type'])
+      return $this->_showXML();
 
-		else
-			return $this->_show();
+    else
+      return $this->_show();
   }
 
-	/**
-	 * Just a backup method to show entry as XML.
-	 *
-	 * @access protected
-	 * @return string XML
-	 *
-	 */
-	protected function _showXML() {
-		return Helper::redirectTo('/errors/404');
-	}
+  /**
+   * Just a backup method to show entry as XML.
+   *
+   * @access protected
+   * @return string XML
+   *
+   */
+  protected function _showXML() {
+    return Helper::redirectTo('/errors/404');
+  }
 
-	/**
-	 * Just a backup method to show entry as JSON.
-	 *
-	 * @access protected
-	 * @return string json
-	 *
-	 */
-	protected function _showJSON() {
-		exit(json_encode(array('error' => 'There is no JSON handling method.')));
-	}
+  /**
+   * Just a backup method to show entry as JSON.
+   *
+   * @access protected
+   * @return string json
+   *
+   */
+  protected function _showJSON() {
+    exit(json_encode(array('error' => 'There is no JSON handling method.')));
+  }
 
-	/**
+  /**
    * Create an action.
    *
    * Create entry or show form template if we have enough rights.
