d2951ee56fc4239c500288ad4d92a6cd96c67213
Marco Raddatz <github@marcoraddatz.com>
2012-07-12 11:53:50 +0200

    added small documentation

--- app/tests/controllers/SampleTest.php
+++ app/tests/controllers/SampleTest.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This is an example how to create an extension test.
+ *
+ * @link http://github.com/marcoraddatz/candyCMS
+ * @author Marco Raddatz <http://marcoraddatz.com>
+ * @license MIT
+ * @since 2.2
+ *
+ */
+
 namespace CandyCMS\Controllers;
 
 require_once dirname(__FILE__) . '/../../../vendor/candyCMS/tests/CandyControllerTestCase.php';
@@ -14,6 +24,8 @@ class SampleTest extends \CandyCMS\Core\Controllers\CandyControllerTestCase {
    * Sets up the fixture, for example, opens a network connection.
    * This method is called before a test is executed.
    *
+   * @access protected
+   *
    */
   protected function setUp() {
     parent::setUp();
@@ -26,12 +38,17 @@ class SampleTest extends \CandyCMS\Core\Controllers\CandyControllerTestCase {
    * Tears down the fixture, for example, closes a network connection.
    * This method is called after a test is executed.
    *
+   * @access protected
+   *
    */
   protected function tearDown() {
     parent::tearDown();
   }
 
   /**
+   * Open an URL and search for defined text.
+   *
+   * @access public
    *
    */
 	public function testShow() {
@@ -40,6 +57,9 @@ class SampleTest extends \CandyCMS\Core\Controllers\CandyControllerTestCase {
 	}
 
   /**
+   * Use candyCMS' main test cases.
+   *
+   * @access public
    *
    */
   public function testCreate() {
@@ -47,6 +67,9 @@ class SampleTest extends \CandyCMS\Core\Controllers\CandyControllerTestCase {
   }
 
   /**
+   * Use candyCMS' main test cases.
+   *
+   * @access public
    *
    */
   public function testUpdate() {
@@ -54,6 +77,9 @@ class SampleTest extends \CandyCMS\Core\Controllers\CandyControllerTestCase {
   }
 
   /**
+   * Use candyCMS' main test cases.
+   *
+   * @access public
    *
    */
   public function testDestroy() {
