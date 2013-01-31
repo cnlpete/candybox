cd4231cf76e5e3076013302b0cd965acf135a555
Hauke Schade <cnlpete@cnlpete.de>
2013-01-31 20:09:31 +0100

    added some compatibility stuff to facebook plugin, but this ain't finished, as i am not sure about the kind of event that is needed

--- Facebook/Facebook.controller.php
+++ Facebook/Facebook.controller.php
@@ -31,6 +31,39 @@ final class FacebookCMS extends Facebook {
   const IDENTIFIER = 'Facebook';
 
   /**
+   * @var array
+   * @access protected
+   *
+   */
+  protected $_aRequest;
+
+  /**
+   * @var array
+   * @access protected
+   *
+   */
+  protected $_aSession;
+
+  /**
+   * Initialize the plugin and register all needed events.
+   *
+   * @access public
+   * @param array $aRequest alias for the combination of $_GET and $_POST
+   * @param array $aSession alias for $_SESSION
+   * @param object $oPlugins the PluginManager
+   *
+   */
+  public function __construct(&$aRequest, &$aSession, &$oPlugins) {
+    $this->_aRequest  = & $aRequest;
+    $this->_aSession  = & $aSession;
+
+    # now register some events with the pluginmanager
+    $oPlugins->registerSimplePlugin($this);
+    #$oPlugins->registerContentDisplayPlugin($this);
+    # @todo there is no pluginmanager event for this yet
+  }
+
+  /**
    * Get user data.
    *
    * @final
@@ -63,13 +96,12 @@ final class FacebookCMS extends Facebook {
    * @final
    * @access public
    * @param array $aUids
-   * @param array $aSession
    * @return array
    *
    */
-  public final function getUserAvatars($aUids, &$aSession = null) {
+  public final function getUserAvatars($aUids) {
     try {
-      $aFacebookAvatarCache = $aSession ? $aSession['facebookavatars'] : array();
+      $aFacebookAvatarCache = &$aSession['facebookavatars'];
 
       # only query for ids we don't know
       $sUids = '';
@@ -107,12 +139,10 @@ final class FacebookCMS extends Facebook {
    *
    * @final
    * @access public
-   * @param array $aRequest
-   * @param array $aSession
    * @return string HTML
    *
    */
-  public final function show(&$aRequest, &$aSession) {
+  public final function show() {
     $sTemplateDir   = Helper::getPluginTemplateDir(self::IDENTIFIER, 'show');
     $sTemplateFile  = Helper::getTemplateType($sTemplateDir, 'show');
 
