09354f50760d7f061d61e44252edf75b9b00f1e9
marcoraddatz <github@marcoraddatz.com>
2013-04-10 12:29:10 +0200

    added php closing tag

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @link http://github.com/marcoraddatz/candyCMS
  * @author Marco Raddatz <http://marcoraddatz.com>
  * @version 3.0
- * @since 3.0
+ * @since 1.0
  *
  */
 
@@ -14,3 +14,4 @@ define('PATH_STANDARD', dirname(__FILE__));
 
 require PATH_STANDARD . '/vendor/candycms/core/initialize.inc.php';
 
+?>
