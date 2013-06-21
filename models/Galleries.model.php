1b69252ceaa7db937f59314a891a94c15380f487
Hauke Schade <cnlpete@cnlpete.de>
2013-06-21 23:23:57 +0200

    fix for the (un)published feature of gallery albums

--- models/Galleries.model.php
+++ models/Galleries.model.php
@@ -334,6 +334,11 @@ class Galleries extends Main {
    *
    */
   public function create() {
+    $iPublished = isset($this->_aRequest[$this->_sController]['published']) &&
+            $this->_aRequest[$this->_sController]['published'] == true ?
+            1 :
+            0;
+
     try {
       $oQuery = $this->_oDb->prepare("INSERT INTO
                                         " . SQL_PREFIX . "gallery_albums
@@ -385,6 +390,11 @@ class Galleries extends Main {
    *
    */
   public function update($iId) {
+    $iPublished = isset($this->_aRequest[$this->_sController]['published']) &&
+            $this->_aRequest[$this->_sController]['published'] == true ?
+            1 :
+            0;
+
     try {
       $oQuery = $this->_oDb->prepare("UPDATE
                                         " . SQL_PREFIX . "gallery_albums
