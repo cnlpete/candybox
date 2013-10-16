3a0061e1a65fd60a84ef602fcedbe362a907299a
Marco Raddatz <info@marcoraddatz.com>
2013-10-16 13:38:45 +0200

    Updated migrations and corrected SQL Error

---
+++ install/migrations/current/20131016_updated_user_information.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `users` CHANGE `verification_date` `verification_date` DATETIME  NULL  DEFAULT NULL;
+ALTER TABLE `users` CHANGE `password_temporary` `password_temporary` VARCHAR(32)  NULL  DEFAULT NULL;
